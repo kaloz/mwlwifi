ccbfc8e329d4ed3090cc370d337f0d5420aacb41
David Lin <dlin@marvell.com>
2016-12-22 16:31:56 +0800

    Restored the way to access MAC register for 8864.
    In this way, it will get correct value.
    
    Signed-off-by: David Lin <dlin@marvell.com>

--- dev.h
+++ dev.h
@@ -30,6 +30,7 @@
 #define MWL_DRV_VERSION	 "10.3.2.0-20161220"
 
 #define MAC_REG_ADDR(offset)      (offset)
+#define MAC_REG_ADDR_PCI(offset)  ((priv->iobase1 + 0xA000) + offset)
 
 #define MCU_CCA_CNT               MAC_REG_ADDR(0x06A0)
 #define MCU_TXPE_CNT              MAC_REG_ADDR(0x06A4)
