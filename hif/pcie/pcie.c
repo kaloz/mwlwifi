e3974073fbea891541371121065197c7a4700fda
Marek Martins <mrkmrtns@gmail.com>
2019-12-19 18:43:11 +0200

    fix

--- hif/pcie/pcie.c
+++ hif/pcie/pcie.c
@@ -454,12 +454,12 @@ static irqreturn_t pcie_isr(struct ieee80211_hw *hw)
 				pcie_priv->is_rx_schedule = true;
 			}
 		}
-
+		/*
 		if (int_status & MACREG_A2HRIC_BIT_RADAR_DETECT) {
 			wiphy_info(hw->wiphy, "radar detected by firmware\n");
 			ieee80211_radar_detected(hw);
 		}
-
+		*/
 		if (int_status & MACREG_A2HRIC_BIT_QUE_EMPTY) {
 			if (!pcie_priv->is_qe_schedule) {
 				if (time_after(jiffies,
@@ -921,12 +921,12 @@ static irqreturn_t pcie_isr_ndp(struct ieee80211_hw *hw)
 				pcie_priv->is_rx_schedule = true;
 			}
 		}
-
+		/*
 		if (int_status & MACREG_A2HRIC_NEWDP_DFS) {
 			wiphy_info(hw->wiphy, "radar detected by firmware\n");
 			ieee80211_radar_detected(hw);
 		}
-
+		*/
 		if (int_status & MACREG_A2HRIC_NEWDP_CHANNEL_SWITCH)
 			ieee80211_queue_work(hw, &priv->chnl_switch_handle);
 	}
